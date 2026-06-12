1f64ee009f07d8531c376163d45b7b3882d3832a
white66-7 <3061690367@qq.com>
2026-06-12 17:46:45 +0800

    feat: 添加音乐播放器组件及其功能

--- blog/src/modules/player/components/Mainplayer.vue
+++ blog/src/modules/player/components/Mainplayer.vue
@@ -30,7 +30,32 @@
     <div class="ctrls">
       <button class="cbtn" :class="{ on: audioStore.isShuffle }" @click="audioStore.toggleMode('shuffle')">⇄</button>
       <button class="cbtn" @click="audioStore.prev()">◄◄</button>
-      <button class="cbtn" @click="audioStore.togglePlay()">{{ audioStore.paused ? '▶' : '⏸' }}</button>
+      <button class="cbtn" @click="audioStore.togglePlay()">
+  <div class="container">
+    <svg
+      v-if="audioStore.paused"
+      class="play"
+      xmlns="http://www.w3.org/2000/svg"
+      height="1em"
+      viewBox="0 0 384 512"
+    >
+      <path
+        d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"
+      />
+    </svg>
+    <svg
+      v-else
+      class="pause"
+      xmlns="http://www.w3.org/2000/svg"
+      height="1em"
+      viewBox="0 0 320 512"
+    >
+      <path
+        d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"
+      />
+    </svg>
+  </div>
+</button>
       <button class="cbtn" @click="audioStore.next()">►►</button>
       <button class="cbtn" :class="{ on: audioStore.isLoop }" @click="audioStore.toggleMode('loop')">↻</button>
     </div>
@@ -240,6 +265,35 @@ function onSetVolume(e: MouseEvent) {
   font-family: inherit;
 }
 
+.container {
+  --color: white;
+  --size: 45px;
+  width: var(--size);
+  height: var(--size);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+  user-select: none;
+}
+.container .play,
+.container .pause {
+  position: absolute;
+  width: 70%;      
+  height: auto;
+  fill: var(--color);
+  animation: keyframes-fill 0.3s;
+}
+@keyframes keyframes-fill {
+  0% {
+    transform: scale(0);
+    opacity: 0;
+  }
+  50% {
+    transform: scale(1.1);
+  }
+}
 .vol {
   display: flex;
   align-items: center;
