0afc7f8eae85fe3abafc0655aa9502c16ef353b5
white66-7 <rodriguezdanielbnofr5850@gmail.com>
2026-06-12 17:46:45 +0800

    feat: 添加音乐播放器组件及其功能

---
+++ blog/src/views/bloghome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <!-- <informatiom /> -->
+    <player />
+    <!-- <photo-show /> -->
+    <!-- <article-display /> -->
+  </div>
+</template>
+
+<script setup lang="ts">
+import player from '@/modules/bloghome/components/music.vue'
+</script>
+
+
+<style>
+
+
+</style>
